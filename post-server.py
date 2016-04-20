b3abdaf5163415f3955b4876d52671b31b03b26c
Karthik Sivaraman <ksivaram@cisco.com>
2016-04-20 16:01:42 -0400

    Initial commit for Python POST Server

---
+++ post-server.py
@@ -0,0 +1,32 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import ssl 
+
+class Server(BaseHTTPRequestHandler):
+    def do_POST(self):
+        print self.headers
+        self.post_data = self.rfile.read(int(self.headers['Content-Length']))
+        
+        self.send_response(200)
+        self.end_headers()
+
+        filename = self.headers.get('X-filename', 'default') + '.gz'
+        with open(filename, 'w') as fout:
+            fout.write(self.post_data)
+        return
+
+
+def run(server_class=HTTPServer, handler_class=Server, port=8443):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    httpd.socket = ssl.wrap_socket (httpd.socket, certfile='server.pem', server_side=True)
+    print 'Starting HTTPS server at %s ...' %port
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+if len(argv) == 2:
+    run(port=int(argv[1]))
+else:
+    run()
